e6b5a3d261b624d9a1d64de4c2f8a9aed1325ca1
& Tony Ta <pair+chrisscherer.tonyta@devbootcamp.com>
2014-04-02 16:34:21 -0500

    functioning landing page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,16 +5,17 @@ feature 'the landing page' do
     user = User.create(email: 'team@greg.com', password: 'password', password_confirmation: 'password')
 
     visit '/'
-    fill_in 'user[email]', with: 'team@greg.com'
-    fill_in 'user[password]', with: 'password'
+    fill_in 'session[email]', with: 'team@greg.com'
+    fill_in 'session[password]', with: 'password'
     click_button 'login'
+
 
     expect(current_path).to eq user_path(user)
   end
 
   scenario 'a person can link to the new user page' do
-    click_button 'signup'
+    visit '/'
+    click_link 'signup'
     expect(current_path).to eq new_user_path
   end
-
 end
