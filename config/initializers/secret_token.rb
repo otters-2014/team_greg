74a7f9be4b2154a436d7f37c6ae3467c2025bdab
& Tony Ta <pair+chrisscherer.tonyta@devbootcamp.com>
2014-04-02 14:36:46 -0500

    initial commit: adds rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnowledgeBombs::Application.config.secret_key_base = '5920a6833f4d9630961821ece4c45320b0c10820ddf5a080d814acf1db57cb6fd9e169a1eb9fff2aedc722b5db74a20a1e466bfdd19b68788e9b7fd578369c05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnowledgeBombs::Application.config.session_store :cookie_store, key: '_knowledge_bombs_session'
