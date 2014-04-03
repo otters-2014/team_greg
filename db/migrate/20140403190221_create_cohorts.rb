881a49d69a8f3d9f21cdc585eebc08ae51b018bd
& Tony Ta <pair+chrisscherer.tonyta@devbootcamp.com>
2014-04-03 15:14:36 -0500

    added cohorts and locations

--- db/migrate/20140403190221_create_cohorts.rb
+++ db/migrate/20140403190221_create_cohorts.rb
@@ -2,6 +2,7 @@ class CreateCohorts < ActiveRecord::Migration
   def change
     create_table :cohorts do |t|
       t.string :name
+      t.integer :location_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140403190221) do
-
+ActiveRecord::Schema.define(version: 20140403200608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +25,12 @@ ActiveRecord::Schema.define(version: 20140403190221) do
   create_table "kbombs", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
