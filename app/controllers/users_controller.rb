d21066c2fd5edd91b5113e58d11905540dc53341
Tony Ta <tonyta.tt@gmail.com>
2014-04-03 14:33:30 -0500

    adds and changes that support features for users and their kbombs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+  end
+
+  private
+
+  def set_user
+    # User.
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :user_kbombs
+  has_many :kbombs, through: :user_kbombs
 end
