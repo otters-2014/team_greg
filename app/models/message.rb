b08e1892d2dbc45eaf15449f1562b2937b5b6129
Apprentice <apprentice@dbc-chicago-002.local>
2014-04-04 13:56:32 -0500

    added user/message associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,3 @@
 class Message < ActiveRecord::Base
+  belongs_to :sender, class_name "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :user_kbombs
   has_many :kbombs, through: :user_kbombs
+
+  has_many :messages, foreign_key "sender_id"
 end
