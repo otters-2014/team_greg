baea3aeed93736ddf4553c5d6fd61afa4ec442f1
Greg Santikian <gsantikian@gmail.com>
2014-04-05 17:09:02 -0500

    Added user validations and fixed syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   belongs_to :cohort
 
   has_many :user_kbombs
