62b11566fb655c43e3117f5a2b1c7cf197e204b0
Greg Santikian <gsantikian@gmail.com>
2014-04-05 18:05:49 -0500

    change tests to make them pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signing up!"
+      redirect_to user_path(@user), notice: "Thank you for signing up!"
     else
       render 'new'
     end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
 feature 'on the landing page' do
+  scenario 'a person can link to the new user page' do
+    visit '/'
+    click_link 'Sign Up'
+    expect(current_path).to eq signup_path
+  end
+end
+
+feature 'on the login page' do
   scenario 'a user can login to see their profile page' do
     user = User.create(email: 'team@greg.com', password: 'password', password_confirmation: 'password')
 
-    visit '/'
-    fill_in 'login[email]', with: 'team@greg.com'
-    fill_in 'login[password]', with: 'password'
+    visit '/login'
+    fill_in 'email', with: 'team@greg.com'
+    fill_in 'password', with: 'password'
     click_button 'login'
 
     expect(current_path).to eq user_path(user)
-  end
-
-  scenario 'a person can link to the new user page' do
-    visit '/'
-    click_link 'signup'
-    expect(current_path).to eq new_user_path
   end
 end
 
